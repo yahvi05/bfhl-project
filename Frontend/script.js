0e61b0b5e7599c071dc8593c85135627651bd906
Yahvi Sethuraman <yahvi0531@gmail.com>
2026-04-24 19:20:19 +0530

    fixed fetch url

--- Frontend/script.js
+++ Frontend/script.js
@@ -2,7 +2,7 @@ async function send() {
     const raw = document.getElementById("input").value;
     const data = raw.split(",").map(item => item.trim());
 
-    const res = await fetch("http://localhost:3000/bfhl", {
+    const res = await fetch("https://bfhl-backend-mv4l.onrender.com/process", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ data })
